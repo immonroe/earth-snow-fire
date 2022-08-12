f1c20e8e31bfec792295a348601bd0c0f11e44a9
immonroe <immanuelmonroe@gmail.com>
2022-08-12 19:15:03 -0400

    Removed pseudocode and went back to first step

--- script.js
+++ script.js
@@ -19,30 +19,3 @@ function getComputerChoice() {
     const choice = ['earth', 'snow', 'fire'];
     return choice[Math.floor(Math.random() * choice.length)];
 }
-
-
-// Write a function that plays a single round of Rock Paper 
-// Scissors. The function should take two parameters - the 
-// playerSelection and computerSelection - and then return a
-//  string that declares the winner of the round like so: 
-//  "You Lose! Paper beats Rock"
-
-// Make your function’s playerSelection parameter case-insensitive 
-// (so users can input rock, ROCK, RocK or any other variation).
-
-// function playRound(playerSelection, computerSelection) {
-//     // your code here!.. You can use an if else statement for this!
-//   }
-   
-//   const playerSelection = "rock";
-//   const computerSelection = getComputerChoice();
-//   console.log(playRound(playerSelection, computerSelection));
-
-// Write a NEW function called game(). Call the playRound function 
-// inside of this one to play a 5 round game that keeps score and 
-// reports a winner or loser at the end.
-// HINT: Remember loops? This is a great opportunity to use one
-
-// for (let i = 0; i < 5; i++) {
-//     // your code here!
-//  }
