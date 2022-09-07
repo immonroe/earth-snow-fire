1758f9b61350b6b3c18fde49ff68a2cab2d3d672
immonroe <immanuelmonroe@gmail.com>
2022-09-07 13:25:09 -0400

    Added DOM to playRound/event listeners

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-// Found a solution using an array and using the Math.random method to pick random object in the array
-// https://stackoverflow.com/questions/5915096/get-a-random-item-from-a-javascript-array
-// https://stackoverflow.com/questions/28737679/rock-paper-scissor-javascript-using-arrays-beginner
-
-// (rock beats scissors)
-// (scissors beats paper)
-// (paper beats rock)
-
 // Add a way to keep track of score throughout the game
 let playerScore = 0;
 let computerScore = 0;
 let draws = 0;
-const rockButton = document.querySelector('#rock')
-const paperButton = document.querySelector('#paper')
-const scissorsButton = document.querySelector('#scissors')
+
+//Create messages for in-game win/loss/draw/error
+let playerWinRound = 'Player has won the round!'
+let computerWinRound = 'CPU has won the round!'
+let draw = `It's a draw!`
+let playerWin = 'Congrats, you have won the game!'
+let computerWin = 'The CPU has won the game!'
+let userError = 'Sorry, you did not select one of the three options. Try again.'
+
+const rockButton = document.querySelector('.rock')
+const paperButton = document.querySelector('.paper')
+const scissorsButton = document.querySelector('.scissors')
+const outcomeDiv = document.querySelector('.outcome')
 
 // Your game is going to play against the computer, 
 // so begin with a function called getComputerChoice that 
@@ -26,54 +28,75 @@ function getComputerChoice() {
 // the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: 
 // "You Lose! x beats y"
 function playRound(playerSelection, computerSelection) {
+  console.log('1 ', playerSelection, '2 ', computerSelection)
     if (playerSelection === computerSelection) {
-        return draw;
+      const p = document.createElement('p')
+      p.innerText = draw;
+      outcomeDiv.appendChild(p)
     } else if (playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'paper' && computerSelection=== 'rock' ||playerSelection === 'scissors' &&computerSelection === 'paper') {
-        return playerWinRound;
+      const p = document.createElement('p')  
+      p.innerText = playerWinRound;
+      outcomeDiv.appendChild(p)
     }
     else if (playerSelection === 'scissors' && computerSelection === 'rock' || playerSelection === 'rock' && computerSelection=== 'paper' ||playerSelection === 'paper' &&computerSelection === 'scissors') {
-        return computerWinRound;
+      const p = document.createElement('p')
+      p.innerText = computerWinRound;
+      outcomeDiv.appendChild(p)
     } else {
-        return userError;
+      const p = document.createElement('p')
+      p.innerText = userError;
+      outcomeDiv.appendChild(p)
     }
   }
 
-//Create messages for in-game win/loss/draw/error
-let playerWinRound = 'Player has won the round!'
-let computerWinRound = 'CPU has won the round!'
-let draw = 'Draw!'
-let playerWin = 'Congrats, you have won the game!'
-let computerWin = 'The CPU has won the game!'
-let userError = 'Sorry, you did not select one of the three options. Try again.'
-
-// Using a for loop to play multiple rounds of the game, the game will also console.log() the score after each round
-for (let i = 0; i < 999; i++) {
-    let playerChoice = prompt('Which will you choose? Rock, paper, or scissors?').toLowerCase();
-    const computerSelection = getComputerChoice();
-    let roundResult = playRound(playerChoice, computerSelection);
-    console.log(roundResult);
-    gameScore(roundResult);
-    console.log('Your score is ' + playerScore);
-    console.log('The CPU score is ' + computerScore);
-
-    // Game ends when either player or CPU wins 5 rounds total
-    // if (playerScore === 5 || computerScore === 5) {
-      // break;
-    }
-  // }
-
-  // A function that keeps track of the score in the game
-  // Also announces winner of game when either user or CPU wins 5 rounds total
-function gameScore(result) {
   
-    if (result === playerWinRound) {
-      playerScore++;
-    } else if (result === draw) {
-      draws++;
-    } else if (result === computerWinRound) {
-      computerScore++;
-    } else (result === userError)
-      return userError;
+
+rockButton.addEventListener('click', () => {
+  const computerSelection = getComputerChoice()
+  const playerSelection = 'rock'
+  playRound(playerSelection, computerSelection)
+})
+
+paperButton.addEventListener('click', () => {
+  const computerSelection = getComputerChoice()
+  const playerSelection = 'paper'
+  playRound(playerSelection, computerSelection)
+})
+
+scissorsButton.addEventListener('click', () => {
+  const computerSelection = getComputerChoice()
+  const playerSelection = 'scissors'
+  playRound(playerSelection, computerSelection)
+})
+
+// // Using a for loop to play multiple rounds of the game, the game will also console.log() the score after each round
+// for (let i = 0; i < 999; i++) {
+//     let playerChoice = prompt('Which will you choose? Rock, paper, or scissors?').toLowerCase();
+//     const computerSelection = getComputerChoice();
+//     let roundResult = playRound(playerChoice, computerSelection);
+//     console.log(roundResult);
+//     gameScore(roundResult);
+//     console.log('Your score is ' + playerScore);
+//     console.log('The CPU score is ' + computerScore);
+
+//     // Game ends when either player or CPU wins 5 rounds total
+//     // if (playerScore === 5 || computerScore === 5) {
+//       // break;
+//     }
+//   // }
+
+//   // A function that keeps track of the score in the game
+//   // Also announces winner of game when either user or CPU wins 5 rounds total
+// function gameScore(result) {
+  
+//     if (result === playerWinRound) {
+//       playerScore++;
+//     } else if (result === draw) {
+//       draws++;
+//     } else if (result === computerWinRound) {
+//       computerScore++;
+//     } else (result === userError)
+//       return userError;
 
   
   // remove logic to play exactly 5 rounds
@@ -85,4 +108,4 @@ function gameScore(result) {
     //   console.log(computerWin);
     //   return;
     // }
-  }
+  // }
