d1a56145f72850c21c42b0831df0b11599a2bbd6
immonroe <immanuelmonroe@gmail.com>
2024-10-18 15:06:56 -0400

    Added single game instance by calling playRound() in dev console

--- script.js
+++ script.js
@@ -1,5 +1,56 @@
+humanScore = 0
+computerScore = 0
+
 function getComputerChoice() {
   const choices = ['rock', 'paper', 'scissors'];
   const randomIndex = Math.floor(Math.random() * 3);
   return choices[randomIndex];
 }
+
+function getHumanChoice() {
+  let choice = prompt('Would you like to pick rock, paper, or scissors?')
+  return choice.toLowerCase()
+}
+
+function playRound(humanChoice, computerChoice) {
+
+  humanChoice = getHumanChoice()
+  computerChoice = getComputerChoice()
+  
+  
+  for (let i = 1; i<= 5; i++)
+  if (humanChoice === computerChoice) {
+    return "It's a tie!";
+  }
+
+  if (humanChoice === "rock") {
+    if (computerChoice === "scissors") {
+      humanScore ++
+      return "You win! Rock beats scissors.";
+    } else {
+      computerScore ++
+      return "You lose! Paper beats rock.";
+    }
+  } else if (humanChoice === "paper") {
+    if (computerChoice === "rock") {
+      humanScore ++
+      return "You win! Paper beats rock.";
+    } else {
+      computerScore ++
+      return "You lose! Scissors beats paper.";
+    }
+  } else if (humanChoice === "scissors") {
+    if (computerChoice === "paper") {
+      humanScore ++
+      return "You win! Scissors beats paper.";
+    } else {
+      computerScore ++
+      return "You lose! Rock beats scissors.";
+    }
+  } else {
+    return 'That is not a valid option, please try again.'
+  }
+}
+
+
+
