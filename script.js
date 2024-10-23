2a918348095accb336570a881c35e5808ac151fc
immonroe <immanuelmonroe@gmail.com>
2024-10-23 19:04:33 -0400

    Reverted changes and removed single letter variable name coverter function

--- script.js
+++ script.js
@@ -4,26 +4,16 @@ let compScore = 0;
 const userScore_span = document.getElementById("user-score");
 const compScore_span = document.getElementById("comp-score");
 
-// Get reference to scoreboard div 
-const scoreBoard_div = document.querySelector(".score-board");
-const result_p = document.querySelector(".result > p"); // specifiying a nested element
-const rock_div = document.getElementById("r");
-const paper_div = document.getElementById("p");
-const scissors_div = document.getElementById("s");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
 
-// Random selected choices from computer
 function getComputerSelection() {
-    const choices=["r","p","s"];
+    const choices=["rock","paper","scissors"];
     const result = Math.floor(Math.random() * 3);
     return choices[result];
 } 
-
-//Converts r,p,s => rock, paper, scissors for output on screen
-function convertToWord(letter) {
-    if (letter === "r") return "Rock";
-    if (letter === "p") return "Paper";
-    return "Scissors";
-}
 
 // Win function, disables buttons at end of game and shows winning message.
 function win(playerSelection, computerSelection) {
@@ -31,73 +21,66 @@ function win(playerSelection, computerSelection) {
     userScore_span.innerHTML = userScore;
     compScore_span.innerHTML = compScore;
 
-    if (userScore < 5){result_p.innerHTML = `${convertToWord(playerSelection)} beats ${convertToWord(computerSelection)}. You win!`;
-} else if(userScore === 5){
-  result_p.innerHTML="Game over, you win! <button onclick='endGame()'>Click here to play again</button>";
+    if (userScore < 5){result_p.innerHTML = ` You win! ${playerSelection} beats ${computerSelection}.`;
+    } else if(userScore === 5){
+    result_p.innerHTML="Congrats, you win! <button onclick='endGame()'>Click here to play again</button>";
 
-  rock_div.setAttribute("disabled", 1);
-  paper_div.setAttribute("disabled", 1);
-  scissors_div.setAttribute("disabled", 1);
+    rock_div.setAttribute("disabled", 1);
+    paper_div.setAttribute("disabled", 1);
+    scissors_div.setAttribute("disabled", 1);
+    }
 }
 
-}
-
-// Lose function, disables buttons at end of game and shows losing message.
 function lose(playerSelection, computerSelection) {
     compScore++;
     userScore_span.innerHTML = userScore;
     compScore_span.innerHTML = compScore;
     
-    if (compScore<5){result_p.innerHTML = `${convertToWord(computerSelection)} beats ${convertToWord(playerSelection)}. You lose!`;
+    if (compScore<5){result_p.innerHTML = `You lose! ${(computerSelection)} beats ${(playerSelection)}.`;
     } else if (compScore === 5) {
-  result_p.innerHTML="Game over, you lose! <button onclick='endGame()'>Play Again!</button>";
-  rock_div.setAttribute("disabled", 1);
-  paper_div.setAttribute("disabled", 1);
-  scissors_div.setAttribute("disabled", 1);
-};
-
+      result_p.innerHTML="Game over, you lose! <button onclick='endGame()'>Play Again?</button>";
+      rock_div.setAttribute("disabled", 1);
+      paper_div.setAttribute("disabled", 1);
+      scissors_div.setAttribute("disabled", 1);
+    }
 }
 
-// Funciton for user and CPU picking the same option
 function draw() {
     userScore_span.innerHTML = userScore;
     compScore_span.innerHTML = compScore;
     result_p.innerHTML = "It's a tie!";
 }
 
-// Game logic, determines win/lose/tie
 function game(playerSelection) {
     const computerSelection = getComputerSelection();
     if (playerSelection === computerSelection) {
       draw(playerSelection, computerSelection);
-    } else if (playerSelection === "r" && computerSelection === "s"){
+    } else if (playerSelection === "rock" && computerSelection === "scissors"){
       win(playerSelection, computerSelection);  
-    } else if (playerSelection === "p" && computerSelection === "r"){
+    } else if (playerSelection === "paper" && computerSelection === "rock"){
       win(playerSelection, computerSelection);
-    } else if (playerSelection === "s" && computerSelection === "p"){
+    } else if (playerSelection === "scissors" && computerSelection === "paper"){
       win(playerSelection, computerSelection);
     } else{
       lose(playerSelection, computerSelection);
     }   
 }
 
-// Game end function (disable all buttons and reset score forcing a reset)
 function endGame() {
   userScore = 0;
   compScore = 0;
   userScore_span.innerHTML = userScore;
   compScore_span.innerHTML = compScore;
-  result_p.innerHTML = ``;
+  result_p.innerHTML = '';
   rock_div.removeAttribute("disabled");
   paper_div.removeAttribute("disabled");
   scissors_div.removeAttribute("disabled");
 }
 
-// Event listeners
 function main() {
-  rock_div.addEventListener("click", () => game("r"));
-  paper_div.addEventListener("click", () => game("p"));
-  scissors_div.addEventListener("click", () => game("s"));
+  rock_div.addEventListener("click", () => game("rock"));
+  paper_div.addEventListener("click", () => game("paper"));
+  scissors_div.addEventListener("click", () => game("scissors"));
 }
 
 main();
